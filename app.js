33098eb56cc1d34f814c4b614ecf70680fba4be9
Andrew Schorr <afs35mm@gmail.com>
2018-04-23 21:33:31 -0400

    Added stuff

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express')
 const app = express()
 const db = require('./db-connection');
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(require("body-parser").json());
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -20,10 +23,49 @@ app.get('/dogs', function (req, res, next) {
         res.status(500).send('Something broke!');
         return;
     }
-
     db.query(`SELECT * FROM dogs WHERE breed like "%${query}%"`, function (error, resp, fields) {
         if (error) throw error;
         res.status(200).json(resp);
+    });
+});
+
+app.get('/widgets', function (req, res, next) {
+    db.query('SELECT * FROM widgets', function (error, resp, fields) {
+        if (error) throw error;
+        res.render('widgets', {
+            title: 'hello world',
+            widgets: resp
+        });
+    });
+});
+
+app.post('/widget', function (req, res, next) {
+    db.query('INSERT INTO `widgets` (`name`) VALUES (?)',
+    [req.body.name],
+    function(err, results, fields) {
+        if (!err) {
+            db.query('SELECT * FROM widgets', function (error, resp, fields) {
+                if (error) throw error;
+                res.status(200).json(resp);
+            });
+        } else {
+            throw err;
+        }
+    });
+});
+
+app.delete('/widget', function (req, res, next) {
+    db.query('DELETE FROM `widgets` where id = (?)',
+    [req.body.id],
+    function(err, results, fields) {
+        if (!err) {
+            db.query('SELECT * FROM widgets', function (error, resp, fields) {
+                if (error) throw error;
+                res.status(200).json(resp);
+            });
+        } else {
+            throw err;
+        }
     });
 });
 
